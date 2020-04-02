485fd3abee9e219f4321ce25356ee026ee306b0c
Will <wilmc17@gmail.com>
2020-04-01 19:39:01 -0700

    part 1 setup

--- Part 2/paging.c
+++ Part 2/paging.c
@@ -1,3 +1,9 @@
 int main(){
+    int n;  /* the n lowest significant bits that represent the offset */
+    int m;  /* the next m bits that represent the page number; assume that n+m is always 16 */
+    int v1; /* first virtual address that needs to be mapped to a page number and offset */
+    int v2; /* second virtual address */
+    int addr[128]; /* a sequence of virtual addresses, one per line until the end of the file */
+
     
 }
